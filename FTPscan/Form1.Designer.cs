354c2a9368280f1e8e33c582e9ae8c7002084820
jake hartley <puffleboy101@hotmail.co.uk>
2019-10-13 22:33:07 +0100

    -commit

--- FTPscan/Form1.Designer.cs
+++ FTPscan/Form1.Designer.cs
@@ -28,13 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(88, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "000.000.000.000:00 | 0 files";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Open...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0 / 0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Thread 1:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Thread 2:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Thread 4:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 18);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Thread 3:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(88, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(187, 18);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "000.000.000.000:00 | 0 files";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(88, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(187, 18);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "000.000.000.000:00 | 0 files";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(88, 107);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(187, 18);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "000.000.000.000:00 | 0 files";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(345, 137);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "FTP crawl";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- FTPscan/Form1.cs
+++ FTPscan/Form1.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +19,185 @@ namespace FTPscan
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public NetworkCredential nc = new NetworkCredential("anonymous", "anonymous");
+        public List<String> dataExternal = new List<String> { };
+        public string fileCurrentlyWriting = "";
+        public int totalIPs;
+        public int completeIps;
+        public string[] lines;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] Ofirstsplit = lines.Take(lines.Length / 2).ToArray();
+            string[] Osecondsplit = lines.Skip(lines.Length / 2).ToArray();
+
+            string[] firstsplit = Ofirstsplit.Take(Ofirstsplit.Length / 2).ToArray();
+            string[] secondsplit = Ofirstsplit.Skip(Ofirstsplit.Length / 2).ToArray();
+            string[] thirdsplit = Osecondsplit.Take(Osecondsplit.Length / 2).ToArray();
+            string[] forthsplit = Osecondsplit.Skip(Osecondsplit.Length / 2).ToArray();
+
+            Thread a = new Thread(() => doExist(firstsplit, 1));
+            a.IsBackground = true;
+            a.Start();
+
+            Thread b = new Thread(() => doExist(secondsplit, 2));
+            b.IsBackground = true;
+            b.Start();
+
+            Thread c = new Thread(() => doExist(thirdsplit, 3));
+            c.IsBackground = true;
+            c.Start();
+
+            Thread d = new Thread(() => doExist(forthsplit, 4));
+            d.IsBackground = true;
+            d.Start();
+        }
+
+        public void doExist(string[] ips, int thrid)
+        {
+            foreach (string s in ips)
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    string safeFilename = s.Replace('.', '-') + ".txt";
+                    safeFilename = safeFilename.Replace(':', '-');
+                    fileCurrentlyWriting = Application.StartupPath + "/" + safeFilename;
+                    fileCurrentlyWriting = fileCurrentlyWriting.Replace('\\', '/');
+                    var file = File.Create(fileCurrentlyWriting);
+                    file.Dispose();
+                    dataExternal = new List<String> { };
+                    completeIps++;
+                    label2.Text = completeIps + " / " + lines.Length;
+                }));
+                string root = "ftp://" + s;
+                List<String> data = getFilesInDirectory(root, thrid, s);
+            }
+        }
+
+        public List<String> getFilesInDirectory(string dir, int thrid, string rawIP)
+        {
+            int fileCounter = 0;
+
+            try
+            {
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(dir);
+                request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+                request.UsePassive = false;
+                request.Credentials = nc;
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(responseStream);
+                string dataRaw = reader.ReadToEnd();
+                string[] dataSpl = dataRaw.Split('\n');
+                List<string> dataFinal = new List<String> { };
+                foreach (string str in dataSpl)
+                {
+                    if (str.Length > 0)
+                    {
+                        string tmp = str.Substring(56);
+                        tmp = Regex.Replace(tmp, @"\t|\n|\r", "");
+                        string nextURl = dir + "/" + tmp;
+                        string processedURL = nextURl.Replace("ftp://", "");
+
+                        processedURL = processedURL.Replace(" ", "_");               
+
+                        //Console.WriteLine(processedURL);
+                        this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            dataExternal.Add(processedURL);
+                            File.WriteAllLines(fileCurrentlyWriting, dataExternal);
+                        }));
+
+                        switch (thrid)
+                        {
+                            case 1:
+                                this.Invoke(new MethodInvoker(delegate ()
+                                {
+                                    label1.Text = rawIP.Replace("ftp://", "") + " | " + fileCounter + " files";
+                                }));
+                                break;
+
+                            case 2:
+                                this.Invoke(new MethodInvoker(delegate ()
+                                {
+                                    label7.Text = rawIP.Replace("ftp://", "") + " | " + fileCounter + " files";
+                                }));
+                                break;
+
+                            case 3:
+                                this.Invoke(new MethodInvoker(delegate ()
+                                {
+                                    label8.Text = rawIP.Replace("ftp://", "") + " | " + fileCounter + " files";
+                                }));
+                                break;
+
+                            case 4:
+                                this.Invoke(new MethodInvoker(delegate ()
+                                {
+                                    label9.Text = rawIP.Replace("ftp://", "") + " | " + fileCounter + " files";
+                                }));
+                                break;
+
+                            default:
+                                break; //should never hit this
+                        }
+
+                        fileCounter++;
+                        string sbst = nextURl.Substring(nextURl.Length - 4, 1);
+                        if(!isUrlAFile(processedURL))
+                        {
+                            getFilesInDirectory(nextURl, thrid, rawIP);
+                        }
+                    }
+                }
+
+                return dataFinal;
+            }
+            catch
+            {
+                return new List<String> { };
+            }
+        }
+
+        public bool isUrlAFile(string url)
+        {
+            string[] splitBySlash = url.Split('/');
+            string finalObj = splitBySlash[splitBySlash.Length - 1];
+            string[] splitByDot = finalObj.Split('.');
+            string finalPostDor = splitByDot[splitByDot.Length - 1];
+            if(finalPostDor.Length > 1 && finalPostDor.Length < 5)
+            {
+                //probably a file
+                return true;
+            }
+            else
+            {
+                //probably a folder, or a really weird file idk
+                return false;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Input File";
+                dlg.Filter = "Text Files | *.txt";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    lines = File.ReadAllLines(dlg.FileName);
+                    label2.Text = "0 / " + lines.Length;
+                }
+            }            
         }
     }
 }
